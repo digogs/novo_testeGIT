3bbe0b0d84c95d7bc1afc688ed21d99b5e678f0c
Rodrigo Gomes <digogs@gmail.com>
2021-03-15 14:38:35 -0300

    php.....

--- index.php
+++ index.php
@@ -13,11 +13,8 @@
 </head>
 
 <body>
-    <?php
-    echo date();
-    ?>
     <nav class="navbar navbar-light navbar-expand-lg fixed-top bg-white clean-navbar">
-        <div class="container"><a class="navbar-brand logo" href="#">Brand</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+        <div class="container"><a class="navbar-brand logo" href="#">Brand<div><h4><?php echo date() ?></h4></div></a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navcol-1">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item"><a class="nav-link active" href="index.html">Home</a></li>
